7c66e1739bdabba31b34425fe669969b3140cf9a
WilheimRy <wilheim.ry@gmail.com>
2017-01-05 13:17:04 +1300

    Listing's page

---
+++ RealEstateProject/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using RealEstateProject.Ef;
+
+namespace RealEstateProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db = new RealEstateDBEntities())
+            {
+                var listings = db.realEstateInfoes.ToList();
+
+                
+            }
+
+            return View();
+        }
+    }
+}
